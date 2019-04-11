694c1eb923753ba012ec0735d53ee254bad93e3b
Alan Fransham <alataffective@outlook.com>
2019-04-11 10:34:06 +0100

    Inital commit

---
+++ EntryUnfocus.WPF/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace EntryUnfocus.WPF
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new EntryUnfocus.App());
+        }
+    }
+}
---
+++ EntryUnfocus/EntryUnfocus/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace EntryUnfocus
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var timer = new Timer(3000);
+
+            timer.Elapsed += (sender, args) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Debug.WriteLine("Entry.Unfocus");
+                    Entry.Unfocus();
+                });
+            };
+
+            timer.Start();
+        }
+    }
+}
